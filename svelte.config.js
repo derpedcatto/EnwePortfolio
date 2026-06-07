e01510aaeb2e766d1400c79293b83aa6f84ab37f
derpedcatto <derpedcatto@protonmail.com>
2026-06-07 16:41:10 +0200

    chore: setting up svelte adapter correctly & npm packages update

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
     runes: ({ filename }) =>
-      filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+      filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
   },
   kit: {
     adapter: adapter({
-      fallback: '404.html',
+      pages: "build",
+      assets: "build",
+      fallback: "404.html",
+      precompress: false,
+      strict: true,
     }),
   },
 };
