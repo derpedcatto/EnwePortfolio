a97845a2b8b29a8a10a2a41e9ffb58b79d1f8f3b
derpedcatto <derpedcatto@protonmail.com>
2026-06-08 22:14:55 +0200

    chore: new libraries

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from "@sveltejs/adapter-static";
+import { mdsvex } from "mdsvex";
+import remarkGfm from "remark-gfm";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: [".svelte", ".svx"],
   compilerOptions: {
     runes: ({ filename }) =>
       filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
   },
+  preprocess: mdsvex({
+    remarkPlugins: [remarkGfm],
+  }),
   kit: {
     adapter: adapter({
       pages: "build",
