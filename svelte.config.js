bf5605381ee754bdf6d1efe24a50cbca1da7e77f
derpedcatto <derpedcatto@protonmail.com>
2026-06-15 04:12:32 +0200

    chore: switching to cloudflare & package update

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
     runes: ({ filename }) =>
-      filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+      filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
   },
   kit: {
     adapter: adapter({
-      fallback: '404.html',
+      fallback: "404.html",
     }),
   },
 };
