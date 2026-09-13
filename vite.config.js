80ec81efb64553934dd6939f1c192f0124ab209d
derpedcatto <derpedcatto@protonmail.com>
2026-09-13 04:37:56 +0200

    chore: switching to vanilla CSS with LightningCSS, removing SASS

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { browserslistToTargets } from "lightningcss";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      targets: browserslistToTargets(browserslist(">= 0.25%")),
+      drafts: {
+        customMedia: true,
+      },
+    },
+  },
+  build: {
+    cssMinify: "lightningcss",
+  },
 });
